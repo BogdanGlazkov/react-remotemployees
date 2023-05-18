d066820e2e76bf25e8af56ff20b6507d27357dc6
BogdanGlazkov <bglazkov@i.ua>
2023-05-18 23:32:50 +0300

    Fix data

--- src/api/contactImagesData.js
+++ src/api/contactImagesData.js
@@ -1,25 +1,24 @@
-
-import sofa from '../images/sofa.png';
-import interior1 from '../images/interior1.png';
-import interior2 from '../images/interior2.png';
+import sofa from "../images/sofa.png";
+import interior1 from "../images/interior1.png";
+import interior2 from "../images/interior2.png";
 
 export const concactImagesData = [
   {
     image: sofa,
     width: 308,
-    className: 'contact-section__sofa-image',
+    className: "contact-section__sofa-image",
     image_number: 1,
   },
   {
     image: interior1,
     width: 104,
-    className: 'contact-section__interior-image',
+    className: "contact-section__interior-image",
     image_number: 2,
   },
   {
     image: interior2,
     width: 108,
-    className: 'contact-section__interior-image--second',
+    className: "contact-section__interior-image--second",
     image_number: 3,
   },
 ];
@@ -28,19 +27,19 @@ export const concactImagesDataMobile = [
   {
     image: sofa,
     width: 212,
-    className: 'contact-section__sofa-image',
+    className: "contact-section__sofa-image",
     image_number: 1,
   },
   {
     image: interior1,
     width: 68,
-    className: 'contact-section__interior-image',
+    className: "contact-section__interior-image",
     image_number: 2,
   },
   {
     image: interior2,
     width: 70,
-    className: 'contact-section__interior-image--second',
+    className: "contact-section__interior-image--second",
     image_number: 3,
   },
 ];
--- src/api/swiper.js
+++ src/api/swiper.js
@@ -6,27 +6,26 @@ import man from "../images/man.svg";
 export const SwipeInfo = [
   {
     image: people,
-    title: '1K+ People',
-    text: 'Succesfully Getting Home',
-    width: 342
+    title: "1K+ People",
+    text: "Succesfully Getting Home",
+    width: 342,
   },
   {
     image: home,
-    title: '56 Houses',
-    text: 'Sold Monthly',
-    width: 216
-
+    title: "56 Houses",
+    text: "Sold Monthly",
+    width: 216,
   },
   {
     image: woman,
-    title: '4K+',
-    text: 'People Looking for New Home',
-    width: 298
+    title: "4K+",
+    text: "People Looking for New Home",
+    width: 298,
   },
   {
     image: man,
-    title: 'Addictional',
-    text: 'For Slider',
-    width: 223
-  }
+    title: "Addictional",
+    text: "For Slider",
+    width: 223,
+  },
 ];

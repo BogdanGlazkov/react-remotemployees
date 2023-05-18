c6d6af642ea1484733002820ee17d57b7fc2c976
BogdanGlazkov <bglazkov@i.ua>
2023-05-18 21:39:54 +0300

    Add App component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { MainPage } from './pages/MainPage';
+import './styles/globalStyles.scss';
+
+function App() {
+  return (
+    <div className="app-container">
+      <MainPage />
+    </div>
+  );
+}
+
+export default App;
